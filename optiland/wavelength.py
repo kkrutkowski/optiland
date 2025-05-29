eae7852b5244b5d265b95d1fc8e26ec80799c08c
Kacper Rutkowski <kk.rutkowski@student.uw.edu.pl>
2025-05-30 01:53:22 +0200

    Add files via upload

--- optiland/wavelength.py
+++ optiland/wavelength.py
@@ -163,34 +163,6 @@ class WavelengthGroup:
 
         self.wavelengths.append(Wavelength(value, is_primary, unit))
 
-    def add_wavelengths(self, min_value, max_value, num_wavelengths, unit="um"):
-        """Add new wavelengths corresponding to the geometrically-spaced Chebyshev nodes
-
-        Args:
-            min_value (float): Minimum wavelength value.
-            max_value (float): Maximum wavelength value.
-            num_wavelengths (int) : Minimum number of wavelengths to be added.
-            unit (str, optional): The unit of the wavelength. Default is 'um'.
-
-        """
-        if not isinstance(num_wavelengths, int):
-            raise ValueError("num_wavelengths must be a positive integer")
-
-        if min_value <= 0 or max_value <= 0:
-            raise ValueError("min_value and max_value must be positive")
-
-        num_wavelengths = be.abs(num_wavelengths)
-        if num_wavelengths % 2 == 0:
-            num_wavelengths += 1
-        nodes = be.arange(1, num_wavelengths + 1)
-        nodes = be.cos((2 * nodes - 1) * be.pi / (2 * num_wavelengths))
-        middle = be.sqrt(min_value * max_value)
-        span = 0.5 * be.log2(max_value / min_value)
-
-        for i, node in enumerate(nodes):
-            is_primary = i == num_wavelengths // 2
-            value = middle * 2 ** (-span * node)
-            self.wavelengths.append(Wavelength(value, is_primary, unit))
 
     def get_wavelength(self, wavelength_number):
         """Get the value of a specific wavelength.
@@ -242,3 +214,55 @@ class WavelengthGroup:
             new_group.add_wavelength(**wave_data)
 
         return new_group
+
+def add_wavelengths(wavelength_group, min_value, max_value, num_wavelengths, unit="um", *, sampling="chebyshev", scale="log"):
+    """Add new wavelengths corresponding to the geometrically-spaced Chebyshev nodes
+
+    Args:
+        min_value (float): Minimum wavelength value.
+        max_value (float): Maximum wavelength value.
+        num_wavelengths (int) : Minimum number of wavelengths to be added.
+        unit (str, optional): The unit of the wavelength. Default is 'um'.
+
+    """
+    if not isinstance(num_wavelengths, int) or num_wavelengths%2 == 0 or num_wavelengths <= 0:
+        raise ValueError("num_wavelengths must be an odd positive integer")
+
+    if min_value <= 0 or max_value <= 0:
+        raise ValueError("min_value and max_value must be positive")
+
+    scale = scale.lower()
+    if scale in {"freq", "frequency"}:
+        scale = "frequency"
+    elif scale in {"wavelength"}:
+        scale = "wavelength"
+    elif scale in {"log", "logarithmic"}:
+        scale = "log"
+    else:
+        raise ValueError(f"Unknown scale: {scale!r}")
+
+    if scale=="frequency":
+        power = -1.0
+    elif scale=="wavelength":
+        power = 1.0
+
+    nodes = be.arange(1, num_wavelengths + 1)
+
+    if sampling=="chebyshev":
+        nodes = 1.0 - be.cos((2 * nodes - 1) * be.pi / (2 * num_wavelengths))
+        if scale=="log":
+            middle = be.sqrt(min_value * max_value)
+            span = be.log2(max_value / min_value)
+
+            for i, node in enumerate(nodes):
+                is_primary = i == num_wavelengths // 2
+                value = min_value * 2 ** (0.5 * span * node)
+                wavelength_group.wavelengths.append(Wavelength(value, is_primary, unit))
+        else:
+            min_value = min_value**power
+            max_value = max_value**power
+            span = max_value - min_value
+            for i, node in enumerate(nodes):
+                is_primary = i == num_wavelengths // 2
+                value = min_value + (0.5 * span * node)
+                wavelength_group.wavelengths.append(Wavelength(value**(power), is_primary, unit))
